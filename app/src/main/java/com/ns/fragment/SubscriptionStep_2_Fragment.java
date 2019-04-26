af8980f44fdea16c111f1bef3455497df23e39ab
ASWANI KUMAR <aswanikumar@NITL-003426.local>
2019-04-26 11:59:09 +0530

    THS- input scroll handled

--- app/src/main/java/com/ns/fragment/SubscriptionStep_2_Fragment.java
+++ app/src/main/java/com/ns/fragment/SubscriptionStep_2_Fragment.java
@@ -5,9 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import com.ns.thpremium.R;
 import com.ns.utils.FragmentUtil;
+import com.ns.utils.ResUtil;
+import com.ns.utils.THPConstants;
+import com.ns.utils.TextSpanCallback;
 import com.ns.view.CustomTextView;
 
 public class SubscriptionStep_2_Fragment extends BaseFragmentTHP {
@@ -16,6 +20,11 @@ public class SubscriptionStep_2_Fragment extends BaseFragmentTHP {
 
     private CustomTextView packName_Txt;
     private CustomTextView planValidity_Txt;
+
+    private EditText name_Et;
+    private EditText moNumber_Et;
+    private CustomTextView tc_Txt;
+    private CustomTextView proceed_Txt;
 
     public static SubscriptionStep_2_Fragment getInstance(String from) {
         SubscriptionStep_2_Fragment fragment = new SubscriptionStep_2_Fragment();
@@ -46,6 +55,22 @@ public class SubscriptionStep_2_Fragment extends BaseFragmentTHP {
         packName_Txt = view.findViewById(R.id.packName_Txt);
         planValidity_Txt = view.findViewById(R.id.planValidity_Txt);
 
+        tc_Txt = view.findViewById(R.id.tc_Txt);
+        name_Et = view.findViewById(R.id.name_Et);
+        moNumber_Et = view.findViewById(R.id.moNumber_Et);
+        proceed_Txt = view.findViewById(R.id.proceed_Txt);
+
+
+        // Terms and Conditions click listener
+        ResUtil.doClickSpanForString(getActivity(), "By signing in, you agree to our  ", "Terms and Conditions",
+                tc_Txt, R.color.blueColor_1, new TextSpanCallback() {
+                    @Override
+                    public void onTextSpanClick() {
+                        TCFragment fragment = TCFragment.getInstance(THPConstants.TnC_URL);
+                        FragmentUtil.pushFragmentAnim((AppCompatActivity)getActivity(), R.id.parentLayout,
+                                fragment, FragmentUtil.FRAGMENT_ANIMATION, false);
+                    }
+                });
 
         // Change Button click listener
         view.findViewById(R.id.changeBtn_Txt).setOnClickListener(v->{
